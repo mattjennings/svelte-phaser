9cf2eb493b733be7635f6a86197049284f35af92
matt jennings <mattjennings44@gmail.com>
2020-03-12 22:49:05 -0500

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+    browser: true,
+  },
+  plugins: ['svelte3'],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  rules: {
+    // ...
+  },
+}
