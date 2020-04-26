715e75b7d10af1b9203da86cd46d2d0a11354444
Matt Jennings <mattjennings44@gmail.com>
2020-04-25 22:34:30 -0500

    use jsdoc for properties table in docs

---
+++ docs/theme/components/Properties.svelte
@@ -0,0 +1,80 @@
+<script>
+  /* (!) Restart bandler if this file was edited
+   *
+   *   Properties data structure:
+   *   data = [
+   *       {
+   *           name <string>,
+   *           description <string>,
+   *           attr[
+   *               default<string>,
+   *               types: <string> | [ <string> ]
+   *           ]
+   *       },
+   *       ...
+   *   ]
+   */
+  export let data
+
+  $: sanitizedData = data
+    .map(prop => {
+      // optional props in our components will often be explicitly set to `undefined`
+      // so that we can let phaser decide what the defaults are when it eventually receives
+      // the value. However, they still get parsed as "required" props here,
+      // so we'll look for a special __REQUIRED__ text in the description to determine if it
+      // actually is required
+      const required =
+        !prop.attr.default &&
+        (prop.description && prop.description.match(/__REQUIRED__\n/))
+
+      if (required) {
+        console.log(prop)
+      }
+      // and then we'll remove the __REQUIRED__ from the description
+      const description =
+        prop.description && prop.description.replace(/__REQUIRED__\n\s?/, '')
+
+      return {
+        ...prop,
+        required,
+        description,
+      }
+    })
+    .sort((aProp, bProp) => {
+      // sort required first
+      if (aProp.required && !bProp.required) {
+        return -1
+      }
+
+      if (bProp.required && !aProp.required) {
+        return 1
+      }
+
+      // then sort alphabetically
+      return aProp.name < bProp.name ? -1 : 1
+    })
+
+  function getTypes(types) {
+    if (!types) return ''
+
+    const typeValue = type => {
+      if (!type) return ''
+      if (typeof type === 'string') return type
+
+      return type.map(v => `<i>${v}</i>`).join(',')
+    }
+
+    return types.map(type => `<dfn>${typeValue(type)}</dfn>`).join('')
+  }
+</script>
+
+<dl class="properties">
+  {#each sanitizedData as prop}
+    <dt>{prop.name}{prop.required ? '*' : ''}</dt>
+    <dd>
+      {@html getTypes(prop.attr.types)}
+    </dd>
+    <dd>{prop.attr.default ? prop.attr.default : ''}</dd>
+    <dd>{prop.description ? prop.description : ''}</dd>
+  {/each}
+</dl>
--- packages/svelte-phaser/src/Game.svelte
+++ packages/svelte-phaser/src/Game.svelte
@@ -3,32 +3,165 @@
   import { setContext } from 'svelte'
   import { removeUndefined } from './util'
 
+  /**
+   * Audio configuration
+   * @type {Phaser.Types.Core.AudioConfig}
+   */
   export let audio = undefined
-  export let autoFocus = undefined
-  export let backgroundColor = undefined
-  export let banner = undefined
-  export let canvas = undefined
-  export let context = undefined
-  export let customEnvironment = false
-  export let disableContextMenu = undefined
-  export let fps = undefined
-  export let height = undefined
-  export let images = undefined
-  export let loader = undefined
-  export let physics = undefined
-  export let plugins = undefined
-  export let render = undefined
-  export let resolution = undefined
-  export let scale = undefined
-  export let seed = undefined
-  export let title = undefined
-  export let transparent = undefined
-  export let type = Phaser.AUTO
-  export let url = undefined
-  export let version = undefined
-  export let width = undefined
-  export let zoom = undefined
 
+  /**
+   * Automatically call window.focus() when the game boots. Usually necessary to capture input events if the game is in a separate frame.
+   * @type {boolean}
+   */
+  export let autoFocus = false
+
+  /**
+   * The background color of the game canvas. The default is black.
+   * @type {string|number}
+   */
+  export let backgroundColor = 0x000000
+
+  /**
+   * Configuration for the banner printed in the browser console when the game starts.
+   * @type {boolean|Phaser.Types.Core.BannerConfig}
+   * @required
+   */
+  export let banner = false
+
+  /**
+   * Provide your own Canvas element for Phaser to use instead of creating one.
+   * You will also need to set the type to Phaser.CANVAS
+   * @type {HTMLCanvasElement}
+   */
+  export let canvas = null
+
+  /**
+   * Provide your own Canvas Context for Phaser to use, instead of creating one.
+   * @type {CanvasRenderingContext2D}
+   */
+  export let context = undefined
+
+  /**
+   * Is Phaser running under a custom (non-native web) environment?
+   * If so, set this to true to skip internal Feature detection.
+   * If true the renderType cannot be left as AUTO.
+   * @type {boolean}
+   */
+  export let customEnvironment = false
+
+  /**
+   * Disable the browser's default 'contextmenu' event (usually triggered by a right-button mouse click).
+   * @type {boolean}
+   */
+  export let disableContextMenu = false
+
+  /**
+   * Game loop configuration.
+   * @type {Phaser.Types.Core.FPSConfig}
+   */
+  export let fps = undefined
+
+  /**
+   * The height of the game, in game pixels.
+   * @type {number}
+   */
+  export let height = 1024
+
+  /**
+   * Images configuration
+   * @type {Phaser.Types.Core.ImagesConfig}
+   */
+  export let images = undefined
+
+  /**
+   * Loader configuration
+   * @type {Phaser.Types.Core.LoaderConfig}
+   */
+  export let loader = undefined
+
+  /**
+   * Physics configuration
+   * @type {Phaser.Types.Core.PhysicsConfig}
+   */
+  export let physics = undefined
+
+  /**
+   *  Plugins to install.
+   * @type {Phaser.Types.Core.PluginObject | Phaser.Types.Core.PluginObjectItem[]}
+   */
+  export let plugins = undefined
+
+  /**
+   * Game renderer configuration.
+   * @type {Phaser.Types.Core.RenderConfig}
+   */
+  export let render = undefined
+
+  /**
+   * The size of each game pixel, in canvas pixels. Values larger than 1 are "high" resolution.
+   * @type {number}
+   */
+  export let resolution = 1
+
+  /**
+   * Scale configuration
+   * @type {Phaser.Types.Core.ScaleConfig}
+   */
+  export let scale = 1
+
+  /**
+   * Seed for the random number generator.
+   * @type {string[]}
+   */
+  export let seed = undefined
+
+  /**
+   * The title of the game. Shown in the browser console.
+   * @type {string}
+   */
+  export let title = undefined
+
+  /**
+   *  Whether the game canvas will have a transparent background
+   * @type {boolean}
+   */
+  export let transparent = false
+
+  /**
+   * Which renderer to use. Phaser.AUTO, Phaser.CANVAS, Phaser.HEADLESS,
+   * or Phaser.WEBGL. AUTO picks WEBGL if available, otherwise CANVAS.
+   * @type {number}
+   */
+  export let type = Phaser.AUTO
+
+  /**
+   * The URL of the game. Shown in the browser console.
+   * @type {string}
+   */
+  export let url = undefined
+
+  /**
+   * The version of the game. Shown in the browser console.
+   * @type {string}
+   */
+  export let version = undefined
+
+  /**
+   * The width of the game, in game pixels.
+   * @type {number}
+   */
+  export let width = 1024
+
+  /**
+   * Simple scale applied to the game canvas. 2 is double size, 0.5 is half size, etc.
+   * @type {number}
+   */
+  export let zoom = 1
+
+  /**
+   * If yo have an existing Phaser.Game instnace you can pass it in here.
+   * @type {}
+   */
   export let instance = new Phaser.Game(
     removeUndefined({
       canvas,
@@ -59,7 +192,7 @@
     })
   )
 
-  let booting = true
+  let booting = !instance.isBooted
 
   instance.events.on('ready', () => {
     booting = false
