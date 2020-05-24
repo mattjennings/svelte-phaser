10f5ec188646a2f3f10fb460101aac354584540a
Matt Jennings <mattjennings44@gmail.com>
2020-05-24 18:49:18 -0500

    Mask

---
+++ packages/svelte-phaser/src/phaser-components/Mask.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { getGameObject } from '../getGameObject'
+  import { onGameEvent } from '../onGameEvent'
+  import { shouldApplyProps } from '../util'
+
+  /**
+   * The Mask this Game Object is using during render.
+   * @type {Phaser.Display.Masks.BitmapMask | Phaser.Display.Masks.GeometryMask}
+   */
+  export let mask = undefined
+
+  const instance = getGameObject()
+
+  $: if (shouldApplyProps(mask)) {
+    if (mask) {
+      instance.setMask(mask)
+    } else {
+      instance.clearMask()
+    }
+  }
+
+  onGameEvent('prestep', () => {
+    mask = instance.mask
+  })
+</script>
