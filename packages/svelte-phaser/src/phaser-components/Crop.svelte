84ef74fb8f76e38f78842d755aecb8749ebd1d99
Matt Jennings <mattjennings44@gmail.com>
2020-05-24 18:05:56 -0500

    Crop component

---
+++ packages/svelte-phaser/src/phaser-components/Crop.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { getGameObject } from '../getGameObject'
+  import { shouldApplyProps } from '../util'
+
+  /**
+   * Applies a crop to a texture based Game Object, such as a Sprite or Image.
+   *
+   * The object should contain x, y, width and height values.
+   * @type {object}
+   */
+  export let crop = undefined
+
+  const instance = getGameObject()
+
+  $: shouldApplyProps(crop) &&
+    instance.setCrop(crop.x, crop.y, crop.width, crop.height)
+</script>
