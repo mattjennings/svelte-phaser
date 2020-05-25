e129b4376198f8fb0d76e7f7cb3a2cc4016fa52b
Matt Jennings <mattjennings44@gmail.com>
2020-05-25 00:23:21 -0500

    ScrollFactor

---
+++ packages/svelte-phaser/src/phaser-components/ScrollFactor.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { getGameObject } from '../getGameObject'
+  import { onGameEvent } from '../onGameEvent'
+  import { shouldApplyProps } from '../util'
+
+  /**
+   * The horizontal scroll factor of this Game Object.
+   *
+   * The scroll factor controls the influence of the movement of a Camera upon this Game Object.
+   *
+   * When a camera scrolls it will change the location at which this Game Object is rendered on-screen. It does not change the Game Objects actual position values.
+   *
+   * A value of 1 means it will move exactly in sync with a camera. A value of 0 means it will not move at all, even if the camera moves. Other values control the degree to which the camera movement is mapped to this Game Object
+   *
+   * Please be aware that scroll factor values other than 1 are not taken in to consideration when calculating physics collisions. Bodies always collide based on their world position, but changing the scroll factor is a visual adjustment to where the textures are rendered, which can offset them from physics bodies if not accounted for in your code.
+   *
+   * #phaserDefault 1
+   * @type {number}
+   */
+  export let scrollFactorX = undefined
+
+  /**
+   * The vertical scroll factor of this Game Object.
+   *
+   * The scroll factor controls the influence of the movement of a Camera upon this Game Object.
+   *
+   * When a camera scrolls it will change the location at which this Game Object is rendered on-screen. It does not change the Game Objects actual position values.
+   *
+   * A value of 1 means it will move exactly in sync with a camera. A value of 0 means it will not move at all, even if the camera moves. Other values control the degree to which the camera movement is mapped to this Game Object.
+   *
+   * Please be aware that scroll factor values other than 1 are not taken in to consideration when calculating physics collisions. Bodies always collide based on their world position, but changing the scroll factor is a visual adjustment to where the textures are rendered, which can offset them from physics bodies if not accounted for in your code.
+   *
+   * #phaserDefault 1
+   * @type {number}
+   */
+  export let scrollFactorY = undefined
+
+  const instance = getGameObject()
+
+  $: if (shouldApplyProps(scrollFactorX, scrollFactorY)) {
+    instance.setScrollFactor(scrollFactorX, scrollFactorY)
+  }
+
+  onGameEvent('prestep', () => {
+    scrollFactorX = instance.scrollFactorX
+    scrollFactorY = instance.scrollFactorY
+  })
+</script>
