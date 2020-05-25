d9e163b9e261aa68225798b8bbc348d7963189da
Matt Jennings <mattjennings44@gmail.com>
2020-05-25 00:21:14 -0500

    Pipeline component

---
+++ packages/svelte-phaser/src/phaser-components/Pipeline.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { getGameObject } from '../getGameObject'
+  import { onGameEvent } from '../onGameEvent'
+  import { shouldApplyProps } from '../util'
+
+  /**
+   * Sets the active WebGL Pipeline of this Game Object.
+   * @type {string}
+   */
+  export let pipeline = undefined
+
+  const instance = getGameObject()
+
+  $: shouldApplyProps(pipeline) && instance.setPipeline(pipeline)
+
+  onGameEvent('prestep', () => {
+    pipeline = instance.pipeline
+  })
+</script>
