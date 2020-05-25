a5490ee7325d0396afc2d2b2c87028f9937b76c1
Matt Jennings <mattjennings44@gmail.com>
2020-05-25 00:36:11 -0500

    Visible

---
+++ packages/svelte-phaser/src/phaser-components/Visible.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { getGameObject } from '../getGameObject'
+  import { onGameEvent } from '../onGameEvent'
+  import { shouldApplyProps } from '../util'
+
+  /**
+   * Sets the visibility of this Game Object.
+   *
+   * An invisible Game Object will skip rendering, but will still process update logic.
+   *
+   * #phaserDefault true
+   * @type {boolean}
+   */
+  export let visible = undefined
+
+  const instance = getGameObject()
+
+  $: shouldApplyProps(visible) && instance.setVisible(visible)
+
+  onGameEvent('prestep', () => {
+    visible = instance.visible
+  })
+</script>
