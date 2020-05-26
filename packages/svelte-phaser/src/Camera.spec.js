48a4a613a69942e6e7de3a37b28b5a2643ce6599
Matt Jennings <mattjennings44@gmail.com>
2020-05-25 19:44:39 -0500

    cleanup camera tests

--- packages/svelte-phaser/src/Camera.spec.js
+++ packages/svelte-phaser/src/Camera.spec.js
@@ -35,27 +35,6 @@ beforeAll(async () => {
   })
 })
 
-afterEach(() => {
-  // scene.cameras.main.destroy()
-})
-
-test('alpha props', async () => {
-  const {
-    component: { instance },
-  } = render(Camera, {
-    ...baseProps,
-    alphaBottomLeft: 0.1,
-    alphaBottomRight: 0.2,
-    alphaTopLeft: 0.3,
-    alphaTopRight: 0.4,
-  })
-
-  expect(instance.alphaBottomLeft).toEqual(0.1)
-  expect(instance.alphaBottomRight).toEqual(0.2)
-  expect(instance.alphaTopLeft).toEqual(0.3)
-  expect(instance.alphaTopRight).toEqual(0.4)
-})
-
 test('bounds props', async () => {
   const {
     component: { instance, $set },
@@ -88,19 +67,6 @@ test('effects props', async () => {
   expect(instance.panEffect).toEqual(3)
   expect(instance.shakeEffect).toEqual(4)
   expect(instance.zoomEffect).toEqual(5)
-})
-
-test('flip props', async () => {
-  const {
-    component: { instance },
-  } = render(Camera, {
-    ...baseProps,
-    flipX: true,
-    flipY: true,
-  })
-
-  expect(instance.flipX).toEqual(true)
-  expect(instance.flipY).toEqual(true)
 })
 
 describe('follow', () => {
@@ -226,19 +192,6 @@ test('roundPixels', async () => {
   expect(instance.roundPixels).toEqual(true)
 })
 
-test('origin props', async () => {
-  const {
-    component: { instance },
-  } = render(Camera, {
-    ...baseProps,
-    originX: 0.5,
-    originY: 0.75,
-  })
-
-  expect(instance.originX).toEqual(0.5)
-  expect(instance.originY).toEqual(0.75)
-})
-
 test('width/height', async () => {
   const {
     component: { instance },
@@ -250,34 +203,4 @@ test('width/height', async () => {
 
   expect(instance.width).toEqual(10)
   expect(instance.height).toEqual(10)
-})
-
-test('tint props', async () => {
-  const {
-    component: { instance, $set },
-  } = render(Camera, { ...baseProps })
-
-  const setTint = jest.spyOn(instance, 'setTint')
-
-  $set({
-    tintTopLeft: 1,
-    tintTopRight: 2,
-    tintBottomLeft: 3,
-    tintBottomRight: 4,
-    tintFill: 100,
-  })
-  await tick()
-
-  expect(setTint).toHaveBeenCalledWith(1, 2, 3, 4, 100)
-})
-
-test('visible props', async () => {
-  const {
-    component: { instance },
-  } = render(Camera, {
-    ...baseProps,
-    visible: false,
-  })
-
-  expect(instance.visible).toEqual(false)
 })
