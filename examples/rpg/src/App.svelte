5c47885c08cd861966955f207ff373afbae061ac
Matt Jennings <mattjennings44@gmail.com>
2020-07-05 12:16:06 -0500

    experimental html UI

--- examples/rpg/src/App.svelte
+++ examples/rpg/src/App.svelte
@@ -6,8 +6,19 @@
   import WorldMap from './scenes/WorldMap.svelte'
 
   let game
-  let camera
+  let uiEl
+  let canvas
+  let style
+
   $: window.game = game
+
+  $: style = canvas && canvas.style
+
+  $: window.addEventListener('resize', () => {
+    setTimeout(() => {
+      style = canvas.style
+    })
+  })
 </script>
 
 <style>
@@ -17,16 +28,39 @@
     width: 100%;
     height: 100%;
   }
+
+  main {
+    position: relative;
+  }
+
+  .ui {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
 </style>
 
-<Game
-  bind:instance={game}
-  width={340}
-  height={244}
-  type={Phaser.CANVAS}
-  render={{ pixelArt: true }}
-  physics={{ default: 'arcade' }}
-  scale={{ mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH }}>
-  <SceneRouter initial="Preload" scenes={{ Preload, WorldMap, Battle }} />
+<main>
+  <canvas bind:this={canvas} />
+  <div
+    class="ui"
+    bind:this={uiEl}
+    style={style && `margin-left: ${style.marginLeft}; margin-top: ${style.marginTop}; width: ${style.width}; height: ${style.height}`}>
+    {#if canvas}
+      <Game
+        {canvas}
+        bind:instance={game}
+        width={340}
+        height={244}
+        type={Phaser.CANVAS}
+        render={{ pixelArt: true }}
+        physics={{ default: 'arcade' }}
+        scale={{ mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH }}>
+        <SceneRouter initial="Preload" scenes={{ Preload, WorldMap, Battle }} />
 
-</Game>
+      </Game>
+    {/if}
+  </div>
+</main>
--- examples/rpg/src/scenes/Battle.svelte
+++ examples/rpg/src/scenes/Battle.svelte
@@ -1,8 +1,12 @@
 <script>
   import { Text, Scene, onGameEvent, getSceneRouter } from 'svelte-phaser'
+  import { slide } from 'svelte/transition'
+  import { quintOut } from 'svelte/easing'
 
   const router = getSceneRouter()
   let keys
+  let exited = false
+
   function create(scene) {
     keys = {
       space: scene.input.keyboard.addKey('space'),
@@ -10,12 +14,34 @@
   }
 
   function update() {
-    if (Phaser.Input.Keyboard.JustDown(keys.space)) {
+    if (!exited && Phaser.Input.Keyboard.JustDown(keys.space)) {
+      exited = true
       router.pop()
     }
   }
 </script>
 
+<style>
+  .menu {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 20%;
+    background: blue;
+    border: 3px solid white;
+    border-radius: 5px;
+    margin: 4px;
+    color: white;
+  }
+</style>
+
 <Scene {...$$props} {create} {update}>
-  <Text x={100} y={100} text="hello" color="white" />
+  <!-- <Text x={100} y={100} text="hello" color="white" /> -->
 </Scene>
+
+<!-- <div class="menu" transition:slide={{ easing: quintOut }}>
+  <ul>
+    <li>Attack</li>
+  </ul>
+</div> -->
