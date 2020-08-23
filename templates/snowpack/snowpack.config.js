00df8dfbba9199b5c4eaf452580abe627f8e0928
Matt Jennings <mattjennings44@gmail.com>
2020-08-23 15:19:27 -0500

    update snowpack template

--- templates/snowpack/snowpack.config.js
+++ templates/snowpack/snowpack.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   extends: '@snowpack/app-scripts-svelte',
   devOptions: {},
+  install: ['svelte'],
   installOptions: {
     rollup: {
       dedupe: ['svelte', 'phaser'],
-      plugins: [require('rollup-plugin-svelte')()],
     },
   },
 }
