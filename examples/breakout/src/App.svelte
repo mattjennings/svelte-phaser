013fb025aa91921b76d7d84a5ceb5ded149a8588
Matt Jennings <mattjennings44@gmail.com>
2021-02-10 22:06:27 -0600

    remove unnecessary Phaser.Math.Clamp in example

--- examples/breakout/src/App.svelte
+++ examples/breakout/src/App.svelte
@@ -4,12 +4,24 @@
   import Breakout from './Breakout.svelte'
   import LoadingBar from './LoadingBar.svelte'
 
-  function preload(scene) {    
+  function preload(scene) {
     scene.load.atlas('assets', 'assets/breakout.png', 'assets/breakout.json')
   }
-
-  Phaser.Math.Clamp()
 </script>
+
+<Game
+  width={800}
+  height={800}
+  physics={{ default: 'arcade' }}
+  scale={{ mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH }}
+>
+  <Scene key="main" {preload} let:progress>
+    <slot slot="loading">
+      <LoadingBar x={400} y={400} {progress} />
+    </slot>
+    <Breakout />
+  </Scene>
+</Game>
 
 <style>
   :global(body) {
@@ -19,16 +31,3 @@
     height: 100%;
   }
 </style>
-
-<Game
-  width={800}
-  height={800}
-  physics={{ default: 'arcade' }}
-  scale={{ mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH }}>
-  <Scene key="main" {preload}  let:progress>
-    <slot slot="loading">
-      <LoadingBar x={400} y={400} {progress} />
-    </slot>
-    <Breakout />
-  </Scene>
-</Game>
