fd3a3b6bfcee013a6c68859048ad848543055ce8
Matt Jennings <mattjennings44@gmail.com>
2021-02-12 15:56:28 -0600

    fix accidental git submodules pt1

---
+++ templates/svite1/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Phaser from 'phaser'
+  import { Game, Scene, Text } from 'svelte-phaser'
+</script>
+
+<Game
+  width={400}
+  height={400}
+  scale={{ mode: Phaser.Scale.FIT, autoCenter: Phaser.Scale.CENTER_BOTH }}
+>
+  <Scene key="main">
+    <Text x={80} y={150} text="svelte-phaser with svite" color="white" />
+  </Scene>
+</Game>
+
+<style>
+  :global(body) {
+    margin: 0;
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+</style>
