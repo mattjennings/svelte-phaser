90f7e2108623bbb0b03c955c1f92ed4ee5dbced1
Matt Jennings <mattjennings44@gmail.com>
2021-02-20 23:31:09 -0600

    update snowpack template

--- templates/snowpack/snowpack.config.js
+++ templates/snowpack/snowpack.config.js
@@ -1,10 +1,25 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  extends: '@snowpack/app-scripts-svelte',
-  devOptions: {},
-  install: ['svelte'],
-  installOptions: {
-    rollup: {
-      dedupe: ['svelte', 'phaser'],
-    },
+  mount: {
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
+  },
+  plugins: ['@snowpack/plugin-svelte', '@snowpack/plugin-dotenv'],
+  routes: [
+    /* Example: Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
   },
 }
